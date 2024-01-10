81fc092937dd7e5209dc859b91b1d57a030a40bd
Hoai-lan DINH VU <christiane.dinhvu@gmail.com>
2024-01-10 01:19:00 +0100

    fix files

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 import { setCursor } from "./src/cursors.js";
 
+<<<<<<< HEAD
 console.log(setCursor('star', '#ff0FF0', 32));
+=======
+console.log(setCursor('default', '#ff0FF0', 32));
+>>>>>>> 40af540 (add default cursor and change index.html style display)
